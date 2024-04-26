c431f8934f367ac0874b9e6e67f6fbf5f166313f
rubengglez <rubengglez@gmail.com>
2024-04-26 18:11:04 +0200

    pass cli tests

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,46 @@
+use std::process::exit;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get {
+        /// key
+        key: String,
+    },
+    Set {
+        /// key
+        key: String,
+        /// value
+        value: String,
+    },
+    Rm {
+        /// key
+        key: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Get { key: _ }) => {
+            eprintln!("unimplemented");
+        }
+        Some(Commands::Set { key: _, value: _ }) => {
+            eprintln!("unimplemented");
+        }
+        Some(Commands::Rm { key: _ }) => {
+            eprintln!("unimplemented");
+        }
+        None => {}
+    }
+    exit(-1)
 }
