46f980f580d42c05b7014b1e5bc6b4d7348ff38a
rubengglez <rubengglez@gmail.com>
2024-05-07 18:54:50 +0200

    Part II: New tests and changes for make it build

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, path::PathBuf};
+
+pub type Result<T> = core::result::Result<T, Box<dyn Error>>;
 
 #[derive(Default)]
 pub struct KvStore {
@@ -12,15 +14,21 @@ impl KvStore {
         }
     }
 
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        Ok(KvStore::new())
+    }
+
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.store.insert(key, value);
+        Ok(())
     }
 
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        Ok(self.store.get(&key).cloned())
     }
 
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
+        Ok(())
     }
 }
